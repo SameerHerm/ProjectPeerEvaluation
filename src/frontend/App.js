99d00483ff50ef60d4ba039ed6cd945acf619e85
Preston <pbj2711@gmail.com>
2025-10-19 23:51:16 -0400

    Add 404.html and switch to HashRouter for better routing compatibility

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import CourseManagement from './pages/CourseManagement';
 import LoginPage from './pages/LoginPage';
